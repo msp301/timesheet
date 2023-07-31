d11bcc5e3512c8477f30d76169f3e6ff64cd0256
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2023-07-31 21:52:11 +0100

    Move parsing date heading into its own routine
    A start and trying to keep to 'main' routine tidy.

--- src/main.rs
+++ src/main.rs
@@ -19,23 +19,27 @@ fn main() -> Result<(), Error> {
     for line in reader.lines() {
         let current_line = line?;
         if current_line.starts_with("## ") {
-            println!("Found h2: {}", current_line);
-            let h2_regex = Regex::new(r"## (\w{3}) (\d{1,2})\w{2} (\w+) (\d{4})").unwrap();
-            if h2_regex.is_match(&current_line) {
-                println!("Matched line: {}", current_line);
-
-                let captures = h2_regex.captures(&current_line).unwrap();
-                let day = captures.get(2).unwrap().as_str();
-                let month = captures.get(3).unwrap().as_str();
-                let year = captures.get(4).unwrap().as_str();
-
-                let date_str = format!("{} {} {}", day, month, year);
-                let dt = NaiveDate::parse_from_str(&date_str, "%d %B %Y").unwrap();
-
-                println!("{}", dt);
-            }
+            let date = parse_date_heading(current_line).unwrap();
+            println!("{}" ,date);
         }
     }
 
     Ok(())
+}
+
+fn parse_date_heading(str : String) -> Option<NaiveDate> {
+    let h2_regex = Regex::new(r"## (\w{3}) (\d{1,2})\w{2} (\w+) (\d{4})").unwrap();
+    if h2_regex.is_match(&str) {
+        println!("Matched line: {}", str);
+
+        let captures = h2_regex.captures(&str).unwrap();
+        let day = captures.get(2).unwrap().as_str();
+        let month = captures.get(3).unwrap().as_str();
+        let year = captures.get(4).unwrap().as_str();
+
+        let date_str = format!("{} {} {}", day, month, year);
+        return Some(NaiveDate::parse_from_str(&date_str, "%d %B %Y").unwrap());
+    }
+
+    return None;
 }
