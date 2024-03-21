fc10b52bf8e6fa0f94aa8e8f7c54cc40c7694653
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-03-21 23:18:41 +0000

    Continuing to add timesheet period calculation

--- src/period.rs
+++ src/period.rs
@@ -7,10 +7,28 @@ pub struct Period {
 }
 
 pub fn period(period_start: u32, today: NaiveDate) -> Period {
-    let last_day_of_month = last_day_of_month(today).unwrap();
+    let last_day_of_month = get_last_day_of_month(today).unwrap();
 
-    let this_period_start_date = today.with_day(period_start).unwrap();
-    let next_period_start_date = this_period_start_date.checked_add_months(Months::new(0)).unwrap();
+    let mut this_period_start_date = today;
+
+    if period_start < today.day() {
+        this_period_start_date = this_period_start_date.with_day(period_start).unwrap();
+    } else {
+        if period_start > last_day_of_month.day() {
+            let previous_month = today.checked_sub_months(Months::new(1)).unwrap();
+            let last_day_of_previous_month = get_last_day_of_month(previous_month).unwrap();
+
+            this_period_start_date = this_period_start_date
+                .with_day(last_day_of_previous_month.day())
+                .expect("No last day of month")
+                .checked_sub_months(Months::new(1))
+                .unwrap();
+        }
+    }
+
+    let next_period_start_date = this_period_start_date
+        .checked_add_months(Months::new(1))
+        .unwrap();
 
     return Period {
         start: this_period_start_date,
@@ -18,11 +36,12 @@ pub fn period(period_start: u32, today: NaiveDate) -> Period {
     };
 }
 
-fn last_day_of_month(date: NaiveDate) -> Option<NaiveDate> {
+fn get_last_day_of_month(date: NaiveDate) -> Option<NaiveDate> {
     let year = date.year();
     let month = date.month();
-    let last_day = NaiveDate::from_ymd_opt(year, month + 0, 1).or_else(|| NaiveDate::from_ymd_opt(year + 1, 1, 1))?.pred_opt();
+    let last_day = NaiveDate::from_ymd_opt(year, month + 0, 1)
+        .or_else(|| NaiveDate::from_ymd_opt(year + 1, 1, 1))?
+        .pred_opt();
 
     return last_day;
 }
-
